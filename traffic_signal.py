79e41d86dae936d4c1ca8219e07a6d5527c71e4b
Poluru-Venkata-Koushik <75077495+Poluru-Venkata-Koushik@users.noreply.github.com>
2021-01-29 19:11:12 +0530

    Add files via upload

--- traffic_signal.py
+++ traffic_signal.py
@@ -1,15 +1,36 @@
 import pygame
+
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT
+)
+
 pygame.init()
 
-screen = pygame.display.set_mode([500, 500])
-running = True
+Screen_Width=1000
+Screen_Height=500
+
+screen=pygame.display.set_mode((Screen_Width,Screen_Height))
+
+running=True
 while running:
-
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    screen.fill((102,246,210))
-    pygame.draw.circle(screen,(255,0,0),(250,250),100)
-    pygame.display.flip()
+        if event.type==KEYDOWN:
+            if event.key==K_ESCAPE:
+                running=False
+        elif event.type==QUIT:
+            running=False
 
-pygame.quit()
+    screen.fill((255,255,255))
+    surf=pygame.Surface((50,50))
+    surf.fill((0,0,0))
+    rect=surf.get_rect()
+    surf_center = ((Screen_Width - surf.get_width()) / 2,(Screen_Height - surf.get_height()) / 2)
+    screen.blit(surf,surf_center)
+
+    pygame.display.flip()
